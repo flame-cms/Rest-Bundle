74aaae51231428d07b01c88e4aa18ef7cf762e74
JSifalda <sifalda.jiri@gmail.com>
2013-07-14 13:06:43 +0200

    Add basic api presenter, set mapping

---
+++ ApiModule/Presenters/ApiPresenter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class ApiPresenter
+ *
+ * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date: 06.07.13
+ */
+namespace Flame\CMS\RestBundle\ApiModule\Presenters;
+
+use Flame\Rest\Application\UI\RestPresenter;
+
+abstract class ApiPresenter extends RestPresenter
+{
+
+}
