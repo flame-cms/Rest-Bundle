75e125ef4a109110ae7051bfeadfb5e2c4f043d8
JSifalda <sifalda.jiri@gmail.com>
2013-07-14 16:00:28 +0200

    Fix mapping

--- Router/RestRouter.php
+++ Router/RestRouter.php
@@ -19,7 +19,7 @@ class RestRouter implements IRouter
 	 */
 	public function getRouteList()
 	{
-		$router = new RouteList();
+		$router = new RouteList('Rest');
 		$router[] = new RestRoute('Api:V1', 'json', true);
 		return $router;
 	}
